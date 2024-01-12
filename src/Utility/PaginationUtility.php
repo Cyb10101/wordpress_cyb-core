21117b062cb6088f577572a358b13d0df66f40e7
Cyb10101 <cyb10101@gmail.com>
2024-01-12 02:27:31 +0100

    Update pagination utility

--- src/Utility/PaginationUtility.php
+++ src/Utility/PaginationUtility.php
@@ -2,49 +2,19 @@
 namespace App\Utility;
 
 class PaginationUtility {
-    /**
-     * @var array
-     */
-    protected $settings = [];
+    protected array $settings = [];
+    protected int $count = 1;
+    protected int $limit = 15;
+    protected int $maxPageLinks = 10;
+    protected string $url = '{page}';
+    protected int $page = 1;
+    protected int $pageLast = 1;
 
-    /**
-     * @var int
-     */
-    protected $count = 1;
-
-    /**
-     * @var int
-     */
-    protected $limit = 15;
-
-    /**
-     * @var int
-     */
-    protected $maxPageLinks = 10;
-
-    /**
-     * @var string
-     */
-    protected $url = '{page}';
-
-    /**
-     * @var int
-     */
-    protected $page = 1;
-
-    /**
-     * @var int
-     */
-    protected $pageLast = 1;
-
-    public function __construct($settings = []) {
+    public function __construct(array $settings = []) {
         $this->settings = array_merge($this->getDefaultSettings(), $settings);
     }
 
-    /**
-     * @return array
-     */
-    protected function getDefaultSettings() {
+    protected function getDefaultSettings(): array {
         return [
             'class' => 'pagination',
             'buttonsStartEnd' => true,
@@ -57,11 +27,7 @@ class PaginationUtility {
         ];
     }
 
-    /**
-     * @param array $items
-     * @return array
-     */
-    public function paginateArray($items) {
+    public function paginateArray(array $items): array {
         $itemsPaginated = [];
         for ($i = $this->getStart(); $i < count($items); $i++) {
             $itemsPaginated[] = $items[$i];
@@ -73,14 +39,9 @@ class PaginationUtility {
     }
 
     /**
-     * @param int $count
-     * @param int $limit
-     * @param int $maxPageLinks
      * @param string|array $url '?page={page}' || 'method' || ['class', 'method']
-     * @param int $page
-     * @return $this
      */
-    public function createPagination(int $count, int $limit = 15, int $maxPageLinks = 10, $url = '{page}', int $page, array $settings = []) {
+    public function createPagination(int $count, int $limit = 15, int $maxPageLinks = 10, string|array $url = '{page}', int $page = 1, array $settings = []): self {
         $this->settings = array_merge($this->settings, $settings);
 
         $this->count = $count;
@@ -90,14 +51,10 @@ class PaginationUtility {
         $this->page = $page;
 
         $this->calculate();
-        $this->getPagination();
         return $this;
     }
 
-    /**
-     * @return $this
-     */
-    protected function calculate() {
+    protected function calculate(): self {
         if ($this->maxPageLinks < 0) {
             $this->maxPageLinks = 0;
         }
@@ -112,10 +69,6 @@ class PaginationUtility {
         return $this;
     }
 
-    /**
-     * @param int $page
-     * @return string
-     */
     public function generateUrl(int $page): string {
         if (is_callable($this->url)) {
             return call_user_func_array($this->url, [$page]);
@@ -123,12 +76,7 @@ class PaginationUtility {
         return str_replace('{page}', $page, $this->url);
     }
 
-    /**
-     * @return string
-     */
     public function getPagination(): string {
-        $this->calculate();
-
         if ($this->limit <= 0 || $this->count <= $this->limit) {
             return '';
         }
@@ -141,7 +89,7 @@ class PaginationUtility {
             $content .= '<li class="page-item"><a href="' . $this->generateUrl($this->page - 1) . '" class="page-link">' . $this->settings['labels']['previous'] . '</a></li>';
         } else {
             if ($this->settings['buttonsStartEnd']) {
-                $content .= '<li class="page-item disabled"><span class="page-link">'.$this->settings['labels']['start'].'</span></li>';
+                $content .= '<li class="page-item disabled"><span class="page-link">' . $this->settings['labels']['start'] . '</span></li>';
             }
             $content .= '<li class="page-item disabled"><span class="page-link">' . $this->settings['labels']['previous'].'</span></li>';
         }
@@ -158,7 +106,20 @@ class PaginationUtility {
                 if ($this->page === $i) {
                     $content .= '<li class="page-item active"><span class="page-link">' . $i . '</span></li>';
                 } else {
-                    $content .= '<li class="page-item"><a href="' . $this->generateUrl($i) . '" class="page-link">' . $i . '</a></li>';
+                    $hideResponsive = '';
+                    if ($i !== $this->page) {
+                        $hideResponsive = ' d-none d-sm-inline-block';
+                        if ($i > $this->page + 1 || $i < $this->page - 1) {
+                            $hideResponsive = ' d-none d-md-inline-block';
+                        }
+                        if ($i > $this->page + 2 || $i < $this->page - 2) {
+                            $hideResponsive = ' d-none d-lg-inline-block';
+                        }
+                        if ($i > $this->page + 3 || $i < $this->page - 3) {
+                            $hideResponsive = ' d-none d-xl-inline-block';
+                        }
+                    }
+                    $content .= '<li class="page-item' . $hideResponsive . '"><a href="' . $this->generateUrl($i) . '" class="page-link">'.$i.'</a></li>';
                 }
             }
         }
@@ -181,10 +142,8 @@ class PaginationUtility {
 
     /**
      * Set icons for start, previous, next, end
-     *
-     * @return $this
      */
-    public function setButtonsIcons() {
+    public function setButtonsIcons(): self {
         $defaultSettings = $this->getDefaultSettings();
         $this->settings['labels']['start'] = $defaultSettings['labels']['start'];
         $this->settings['labels']['previous'] = $defaultSettings['labels']['previous'];
@@ -193,11 +152,7 @@ class PaginationUtility {
         return $this;
     }
 
-    /**
-     * @param string $class
-     * @return $this
-     */
-    public function setClass(string $class = '') {
+    public function setClass(string $class = ''): self {
         $defaultSettings = $this->getDefaultSettings();
         $this->settings['class'] = (!empty($class) ? $class : $defaultSettings['class']);
         return $this;
@@ -205,14 +160,8 @@ class PaginationUtility {
 
     /**
      * Set text for icons for start, previous, next, end
-     *
-     * @param string $start
-     * @param string $previous
-     * @param string $next
-     * @param string $end
-     * @return $this
      */
-    public function setButtonsText(string $start = '', string $previous = '', string $next = '', string $end = '') {
+    public function setButtonsText(string $start = '', string $previous = '', string $next = '', string $end = ''): self {
         $this->settings['labels']['start'] = (!empty($start) ? $start : 'Anfang');
         $this->settings['labels']['previous'] = (!empty($previous) ? $previous : 'Zurück');
         $this->settings['labels']['next'] = (!empty($next) ? $next : 'Weiter');
@@ -222,47 +171,29 @@ class PaginationUtility {
 
     /**
      * Enable/disable start ... end buttons
-     *
-     * @param bool $bool
-     * @return $this
      */
-    public function setButtonsStartEnd(bool $bool) {
+    public function setButtonsStartEnd(bool $bool): self {
         $this->settings['buttonsStartEnd'] = $bool;
         return $this;
     }
 
-    /**
-     * @return string
-     */
-    public function getPagesDetails() {
+    public function getPagesDetails(): string {
         return 'Seite ' . $this->page . ' von ' . $this->pageLast;
     }
 
-    /**
-     * @return string
-     */
-    public function getSqlLimit() {
+    public function getSqlLimit(): string {
         return 'LIMIT ' . $this->getStart() . ',' . $this->limit;
     }
 
-    /**
-     * @return int
-     */
-    public function getStart() {
+    public function getStart(): int {
         return ($this->page - 1) * $this->limit;
     }
 
-    /**
-     * @return int
-     */
-    public function getLimit() {
+    public function getLimit(): int {
         return $this->limit;
     }
 
-    /**
-     * @return int
-     */
-    public function getEnd() {
+    public function getEnd(): int {
         return $this->getStart() + $this->limit;
     }
 }
